d270c6c288f997fa9d7791bb55802c348c5da5aa
Can Vural <can9119@gmail.com>
2022-03-07 13:24:09 +0100

    feat: support Laravel 9 style attribute accessors and mutators

--- tests/Features/Properties/ModelPropertyExtension.php
+++ tests/Features/Properties/ModelPropertyExtension.php
@@ -16,6 +16,7 @@ use ArrayObject;
 use Carbon\Carbon as BaseCarbon;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
+use function PHPStan\dumpType;
 
 class ModelPropertyExtension
 {
@@ -146,6 +147,7 @@ class ModelPropertyExtension
 
     public function testDateCast(User $user): ?BaseCarbon
     {
+        dumpType($user->email);
         $user->email_verified_at = now();
 
         return $user->email_verified_at;
